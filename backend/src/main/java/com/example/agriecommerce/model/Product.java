db187d66fc6d5713ef4fa9469d37c93dae5d83d4
Kelly Nyachiro <kellynyachiro@gmail.com>
2025-08-03 12:02:45 +0300

    fixed both product and user to added all the imports

--- backend/src/main/java/com/example/agriecommerce/model/Product.java
+++ backend/src/main/java/com/example/agriecommerce/model/Product.java
@@ -1,8 +1,32 @@
 package com.example.agriecommerce.model;
 
-import javax.persistence.*;
-import javax.validation.constraints.*;
+package com.example.agriecommerce.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import java.math.BigDecimal;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 
 @Entity
 @Table(name = "products")
--- backend/src/main/java/com/example/agriecommerce/model/User.java
+++ backend/src/main/java/com/example/agriecommerce/model/User.java
@@ -1,8 +1,28 @@
 package com.example.agriecommerce.model;
 
-import javax.persistence.*;
-import javax.validation.constraints.*;
-import java.util.*;
+package com.example.agriecommerce.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 
 @Entity
 @Table(name = "users", uniqueConstraints = {
