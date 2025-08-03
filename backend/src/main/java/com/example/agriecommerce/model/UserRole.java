9c895f68d59a3f83bf61f9d8a0ee4d37ab822666
Kelly Nyachiro <kellynyachiro@gmail.com>
2025-08-03 12:11:26 +0300

    added both package com.example.agriecommerce.model;

---
+++ backend/src/main/java/com/example/agriecommerce/model/UserRole.java
@@ -0,0 +1,7 @@
+package com.example.agriecommerce.model;
+
+public enum UserRole {
+    USER,
+    ADMIN,
+    FARMER
+}
---
+++ backend/src/main/java/com/example/agriecommerce/model/UserStatus.java
@@ -0,0 +1,7 @@
+package com.example.agriecommerce.model;
+
+public enum UserStatus {
+    ACTIVE,
+    INACTIVE,
+    BANNED
+}
