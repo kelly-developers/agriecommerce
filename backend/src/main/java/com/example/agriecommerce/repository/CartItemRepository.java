d05fbf4451a4bc9bc7909ada2f87a90117d0f11b
Kelly Nyachiro <kellynyachiro@gmail.com>
2025-08-04 10:18:25 +0300

    latest advancements

---
+++ backend/src/main/java/com/example/agriecommerce/repository/CartItemRepository.java
@@ -0,0 +1,11 @@
+package com.example.agriecommerce.repository;
+
+import com.example.agriecommerce.model.Cart;
+import com.example.agriecommerce.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    void deleteAllByCart(Cart cart);
+}
